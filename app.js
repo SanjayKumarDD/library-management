0574fe367274edcc267eb302e736ec54edaf37a6
SANJAY KUMAR D <92215315+SanjayKumarDD@users.noreply.github.com>
2023-09-23 19:21:36 +0530

    title

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-// const API_URL = "https://www.googleapis.com/books/v1/volumes?q=";
-const IMG_PATH = "";
-const SEARCH_API = "https://www.googleapis.com/books/v1/volumes?q=";
+const API_URL =
+  "https://www.googleapis.com/books/v1/volumes?q=lord%20of%20the%20rings";
+const SEARCH_API =
+  'https://www.googleapis.com/books/v1/volumes?q="';
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
@@ -12,43 +13,29 @@ async function getLibrary(url) {
   const res = await fetch(url);
   const data = await res.json();
 
-  showLibrary(data.items.volumeInfo);
+  showLibrary(data.items);
 }
 
 function showLibrary(Library) {
   main.innerHTML = "";
 
   Library.forEach((book) => {
-    // const { title, authors,imageLinks.thumbnail , averageRating, description } = book
-    const { title, authors, averageRating, description } = book;
+    const { title,authors,averageRating,description } = book.volumeInfo;
 
     const bookEl = document.createElement("div");
     bookEl.classList.add("book");
 
     bookEl.innerHTML = `
-            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            
             <div class="book-info">
           <h3>${title}</h3>
-          <span class="${getClassByRate(vote_average)}">${averageRating}</span>
+          <div>${description}</div>
             </div>
-            <div class="overview">
-          <h3>Overview</h3>
-          ${description}
-        </div>
-        `;
+          `
     main.appendChild(bookEl);
   });
 }
-//rating color
-function getClassByRate(vote) {
-  if (vote >= 4) {
-    return "green";
-  } else if (vote >= 2.5) {
-    return "orange";
-  } else {
-    return "red";
-  }
-}
+
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
