6a8c6aa658e3f3b0864a2f1ca4a1ea43db7a51fd
SANJAY KUMAR D <92215315+SanjayKumarDD@users.noreply.github.com>
2023-09-23 20:02:17 +0530

    description added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const API_URL =
   "https://www.googleapis.com/books/v1/volumes?q=lord%20of%20the%20rings";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API =
   'https://www.googleapis.com/books/v1/volumes?q="';
 
@@ -21,21 +22,33 @@ function showLibrary(Library) {
 
   Library.forEach((book) => {
     const { title,authors,averageRating,description } = book.volumeInfo;
-
+    const {thumbnail} = book.volumeInfo.imageLinks;
     const bookEl = document.createElement("div");
     bookEl.classList.add("book");
 
     bookEl.innerHTML = `
-            
+           <img src = "${thumbnail}" alt="${title}"> 
             <div class="book-info">
           <h3>${title}</h3>
-          <div>${description}</div>
+          <span class = "${getClassByRate(averageRating)}">${averageRating}</span>
+          <div class="overview">
+            <h3>Description</h3>
+            ${description}
             </div>
           `
     main.appendChild(bookEl);
   });
 }
 
+function getClassByRate(vote) {
+  if (vote >= 4) {
+    return "green";
+  } else if (vote >= 2) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
