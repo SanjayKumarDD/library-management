d92c22e05665a5e05788eb9526953eb4a4600ace
SANJAY KUMAR D <92215315+SanjayKumarDD@users.noreply.github.com>
2023-09-24 08:35:00 +0530

    count updated

--- app.js
+++ app.js
@@ -5,30 +5,48 @@ const SEARCH_API = 'https://www.googleapis.com/books/v1/volumes?q="';
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+const but = document.querySelector('#but');
 
 getLibrary(API_URL);
 
 async function getLibrary(url) {
   const res = await fetch(url);
   const data = await res.json();
+  countBooks(data);
   showLibrary(data.items,data);
 }
 
+// but.addEventListener('click',(e) =>{
+//   e.preventDefault();
+//   // getLibrary(API_URL);
+//   const str= "classic";
+//     getLibrary(SEARCH_API + str);
+//   // window.location.reload();
+// })
+
+
 function countBooks(data){
-  const { totalItems } = data;
+  // const { totalItems } = data;
+  const totalItems = data.items.length;
   const count = document.createElement("p");
   count.innerHTML = `<p>About ${totalItems} results</p>`;
   main.appendChild(count);
 }
+
+let Lib, dat;
 function showLibrary(Library,data) {
-  main.innerHTML = "";
-  countBooks(data);
+  //
+  Lib = Library;
+  dat = data;
+  //
+
+  // main.innerHTML = "";
+  // countBooks(data);
   Library.forEach((book) => {
     const { title, authors, averageRating, description } = book.volumeInfo;
     const { thumbnail } = book.volumeInfo.imageLinks;
     const bookEl = document.createElement("div");
     bookEl.classList.add("book");
-
     bookEl.innerHTML = `
            <img src = "${thumbnail}" alt="${title}"> 
            
@@ -50,6 +68,14 @@ function showLibrary(Library,data) {
   });
 }
 
+
+window.addEventListener('scroll',()=>{
+   if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+      showLibrary(Lib,dat);
+   }
+})
+
+
 function getClassByRate(vote) {
   if (vote >= 4) {
     return "green";
@@ -66,6 +92,7 @@ form.addEventListener("submit", (e) => {
   const searchTerm = search.value;
 
   if (searchTerm && searchTerm !== "") {
+    main.innerHTML = "";
     getLibrary(SEARCH_API + searchTerm);
 
     search.value = "";
